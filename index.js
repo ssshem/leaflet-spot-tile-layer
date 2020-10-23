661a1b64f09063388da42b9a3543d212fabe350d
gaos <gaos@webyun.cn>
2020-10-23 15:03:56 +0800

    Revert "引入ol，用ol驱动leaflet来使用，两者一起使用时会有一个bug，ol被拖动的幅度太大，leaflet spot layer create tile会落后于leafmap.moveend、layer.load执行，所以会出现没有绘制色斑图的情况，得在ol.moveend中leafletmap.setView后，触发layer.fire('load')"
    This reverts commit e1a649fc

--- index.js
+++ index.js
@@ -1,65 +1,29 @@
-var olView = new ol.View({
-  center: OlCommon.transformLonLatToCoordinate(109.9, 18.5),
-  zoom: 3,
-  minZoom: 3,
-  maxZoom: 16,
-  projection: OlCommon.EPSG3857
-});
-var olMap = new ol.Map({
-  target: 'map-container',
-  layers: [
-    new ol.layer.Tile({
-      visible: false,
-      crossOrigin: 'anonymous',
-      preload: 1,
-      opacity: 1,
-      source: new ol.source.OSM()
-    })
-  ],
-  view: olView,
-  controls: ol.control.defaults({
-    attributionOptions: {
-      collapsible: false
-    }
-  }),
-  interactions: ol.interaction.defaults({
-    altShiftDragRotate: false,
-    pinchRotate: false,
-    pinchZoom: false
-  }).extend([new ol.interaction.PinchZoom({constrainResolution: true})]),
-});
+// map
+var Lmap = L.map('map-container', {
+  center: [18.5, 109.9],
+  zoom: 2,
+  minZoom: 2
+}).on('click', function() {
+  var popup = L.popup();
+  return function(e) {
+    popup
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(Lmap);
+  };
+}());
 
-
-
-
-
-// // map
-// var Lmap = L.map('map-container', {
-//   center: [18.5, 109.9],
-//   zoom: 2,
-//   minZoom: 2,
-//   inertia: false
-// }).on('click', function() {
-//   var popup = L.popup();
-//   return function(e) {
-//     popup
-//       .setLatLng(e.latlng)
-//       .setContent("You clicked the map at " + e.latlng.toString())
-//       .openOn(Lmap);
-//   };
-// }());
-
-// // layers
-// var darkmap = L.tileLayer('https://tiles.windy.com/tiles/v9.0/darkmap/{z}/{x}/{y}.png', {
-//   maxZoom: 18,
-//   zIndex: 20
-// }).addTo(Lmap);
+// layers
+var darkmap = L.tileLayer('https://tiles.windy.com/tiles/v9.0/darkmap/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  zIndex: 20
+}).addTo(Lmap);
 
 var spotLayer = new SpotGridLayer({
   zIndex: 10,
   debuged: false,
   opacity: 0.9
-}).updateUrl('./data/tiles/pressure/{z}/{x}_{y}', {
+}).addTo(Lmap).updateUrl('./data/tiles/pressure/{z}/{x}_{y}', {
   dataZooms: [2, 5],
   colorSegments: [
     [99000,[142,179,184,255]], [99499.9,[142,179,184,255]],
@@ -88,21 +52,15 @@ var spotLayer = new SpotGridLayer({
   ]
 });
 
-var Lmap = OlCommon.getSingleLMap('leaflet-map-container', olMap);
-
-spotLayer.addTo(Lmap);
-
 var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(Lmap);
 
 // controls
 var baseMap = {
   "OSM": osm
 };
-// var spotLayers = {
-//   'spot': spotLayer,
-//   'darkmap': darkmap
-// };
-// L.control.layers(baseMap, spotLayers).addTo(Lmap);
-
-OlCommon.CountriesLayer.show(olMap);
+var spotLayers = {
+  'spot': spotLayer,
+  'darkmap': darkmap
+};
+L.control.layers(baseMap, spotLayers).addTo(Lmap);
 
